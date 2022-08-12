a21d6952ddf052856df98f462efa8bbf1cae1cd0
lzy99 <1005897034@qq.com>
2022-08-12 23:11:00 +0900

    basic build

--- MainWindow.py
+++ MainWindow.py
@@ -13,6 +13,57 @@ class MainWindow(QWidget):
         self.ui = uic.loadUi("./res/ui/MainWindow.ui")
         self.player = QMediaPlayer()
 
+        # 连接信号与槽
+        self.ui.playBtn.clicked.connect(self.play_video)
+        self.ui.pauseBtn.clicked.connect(self.pause_video)
+        self.ui.fullscreenBtn.clicked.connect(self.full_screen)
+
+        self.ui.openAct.triggered.connect(self.open_act)
+
+        # 注册事件过滤器
+        self.ui.videoWidget.installEventFilter(self)
+
+    # 事件
+    def eventFilter(self, watched, event):
+        if watched != self.ui.videoWidget:
+            return super(MainWindow, self).eventFilter(watched, event)
+        # 鼠标事件
+        if event.type() == QEvent.Type.MouseButtonPress:
+            # 鼠标点击播放/暂停
+            if event.button() == Qt.MouseButton.LeftButton:
+                if self.player.state() == QMediaPlayer.State.PlayingState:
+                    self.player.pause()
+                else:
+                    self.player.play()
+        # 键盘事件
+        if event.type() == QEvent.Type.KeyPress:
+            if event.key() == Qt.Key.Key_Escape:
+                if self.ui.videoWidget.isFullScreen():
+                    self.ui.videoWidget.setFullScreen(False)
+
+        return super(MainWindow, self).eventFilter(watched, event)
+
+    # 动作
+    def open_act(self):
+        self.player.setVideoOutput(self.ui.videoWidget)
+        cap = "open video file"
+        filt = "视频文件(*.wmv *.avi *.mp4 *.mov)"
+        self.player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl(caption=cap, filter=filt)[0]))
+        self.play_video()
+
+    # 按键功能
+    def full_screen(self):
+        if not self.ui.videoWidget.isFullScreen():
+            self.ui.videoWidget.setFullScreen(True)
+
+    def play_video(self):
+        if self.player.state() != QMediaPlayer.State.PlayingState:
+            self.player.play()
+
+    def pause_video(self):
+        if self.player.state() != QMediaPlayer.State.PausedState:
+            self.player.pause()
+
 
 if __name__ == '__main__':
     QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
